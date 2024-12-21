2a3151b657ec4b3ac1dfa99b11f42d35c3206032
FaisalAkinola <faisal.akinola@gmail.com>
2024-12-21 09:30:09 -0500

    e

--- ClassAttendance.py
+++ ClassAttendance.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 datafile=pd.read_csv('ClassAttendance.csv')
+st.set_page_config(page_title='Class Attendance',page_icon=':school:')
 menu=st.sidebar.selectbox('Choose Menu',["Input Info","Info Display"])
+st.sidebar.link_button(url='https://stemhackathon.com/question-1-data-apps-hard/',label='Question')
 if menu=="Input Info":
     st.title("Attendance Tracker")
     col1,col2=st.columns(2)
@@ -12,6 +14,7 @@ if menu=="Input Info":
     with col2:
         present=st.number_input("Enter Number of Present Days",0)
         absent=st.number_input("Enter Number of Absent Days",0)
+        date=st.date_input("Select Today's Date")
     if st.button("Sumbit"):
 
         attendancedict={"Name":[name],"Roll":[roll],"Present":[present],"Absent":[absent]}
